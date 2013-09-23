d7a01371cc4bbed826ae06eb7a877cfdd9a75cf8
Phoebe Schmidt <pbschmidt@berkeley.edu>
2013-09-22 21:58:32 -0700

    begin q2

--- p2/multiagent/multiAgents.py
+++ p2/multiagent/multiAgents.py
@@ -1,5 +1,6 @@
 # multiAgents.py
 # --------------
+<<<<<<< Updated upstream
 # Licensing Information:  You are free to use or extend these projects for
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
@@ -10,6 +11,18 @@
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
 # Student side autograding was added by Brad Miller, Nick Hay, and
+=======
+# Licensing Information:  You are free to use or extend these projects for 
+# educational purposes provided that (1) you do not distribute or publish 
+# solutions, (2) you retain this notice, and (3) you provide clear 
+# attribution to UC Berkeley, including a link to 
+# http://inst.eecs.berkeley.edu/~cs188/pacman/pacman.html
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero 
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and 
+>>>>>>> Stashed changes
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
 
@@ -75,6 +88,12 @@ class ReflexAgent(Agent):
         newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
 
         "*** YOUR CODE HERE ***"
+
+
+        print newPos
+        print newFood
+        print newGhostStates
+        print newScaredTimes
         return successorGameState.getScore()
 
 def scoreEvaluationFunction(currentGameState):
@@ -130,6 +149,10 @@ class MinimaxAgent(MultiAgentSearchAgent):
             Returns the total number of agents in the game
         """
         "*** YOUR CODE HERE ***"
+        numAgents = gameState.getNumAgents()
+        maxDepth = self.depth
+        currentDepth = 0
+        
         util.raiseNotDefined()
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
