b21a70a5fd0128ee9ea685e51b5e749aa1c4d461
Nader Morshed <morshed.nader@gmail.com>
2013-10-08 13:31:31 -0700

    Got q6 working, apparently q5 was already passing

--- p3/reinforcement/analysis.py
+++ p3/reinforcement/analysis.py
@@ -63,7 +63,7 @@ def question3e():
 def question6():
     answerEpsilon = None
     answerLearningRate = None
-    return answerEpsilon, answerLearningRate
+    return 'NOT POSSIBLE'
     # If not possible, return 'NOT POSSIBLE'
 
 if __name__ == '__main__':
