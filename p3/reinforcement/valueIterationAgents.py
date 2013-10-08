009edcdd6c0cc6e129f51ac0d28e71e328b90002
Nader Morshed <morshed.nader@gmail.com>
2013-10-08 11:10:15 -0700

    Fixed a syntax error

--- p3/reinforcement/valueIterationAgents.py
+++ p3/reinforcement/valueIterationAgents.py
@@ -1,15 +1,15 @@
 # valueIterationAgents.py
 # -----------------------
-# Licensing Information:  You are free to use or extend these projects for 
-# educational purposes provided that (1) you do not distribute or publish 
-# solutions, (2) you retain this notice, and (3) you provide clear 
-# attribution to UC Berkeley, including a link to 
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to
 # http://inst.eecs.berkeley.edu/~cs188/pacman/pacman.html
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
-# The core projects and autograders were primarily created by John DeNero 
+# The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# Student side autograding was added by Brad Miller, Nick Hay, and 
+# Student side autograding was added by Brad Miller, Nick Hay, and
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
 
@@ -48,22 +48,20 @@ class ValueIterationAgent(ValueEstimationAgent):
         iteration = 1
         maxVal = null
         while iteration <= iterations:
-          
+
             for state in mdp.getStates():
-              actions = mdp.getPossibleActions(state)
-              if actions == null:
-                "do something here if there are no actions"
-              else:
-                for a in actions:
-                  qval = computeQValueFromValues(state, a)
-                  if maxVal == null:
-                    maxVal = qval
-                  elif qval > maxVal:
-                    maxVal = qval "there has got to be a more elegant python way to write this"
+                actions = mdp.getPossibleActions(state)
+                if actions == null:
+                    "do something here if there are no actions"
+                else:
+                    for a in actions:
+                        qval = computeQValueFromValues(state, a)
+                        if maxVal == null:
+                            maxVal = qval
+                        elif qval > maxVal:
+                            maxVal = qval #"there has got to be a more elegant python way to write this"
 
             i += 1
-
-
 
     def getValue(self, state):
         """
